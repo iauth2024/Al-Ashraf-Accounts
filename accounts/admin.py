86d2d42e45c83ff5554708db456225596cef264e
iauth2024 <azadpashashaik@gmail.com>
2024-09-19 12:37:49 +0530

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -30,3 +30,41 @@ class UserActivityLogAdmin(admin.ModelAdmin):
     list_display = ('user', 'timestamp', 'screen_time', 'work_time')
     list_filter = ('user', 'timestamp')
     search_fields = ('user__username',)
+
+from django.contrib import admin
+from .models import Contra, Balance
+from django.contrib.auth.models import User
+
+class ContraAdmin(admin.ModelAdmin):
+    list_display = ('contra_no', 'amount', 'date', 'contra_type', 'performed_by')
+    list_filter = ('contra_type', 'date', 'performed_by')
+    search_fields = ('contra_no', 'amount', 'date', 'contra_type', 'performed_by__username')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('performed_by')
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # This is a new instance
+            super().save_model(request, obj, form, change)
+            obj.reverse_contra_transaction()
+        else:
+            super().save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        obj.reverse_contra_transaction()
+        super().delete_model(request, obj)
+
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('cash_balance', 'non_cash_balance')
+    readonly_fields = ('cash_balance', 'non_cash_balance')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.none()  # To ensure only one Balance record is shown
+
+    def has_change_permission(self, request, obj=None):
+        return False  # Prevent modification through the admin interface
+
+admin.site.register(Contra, ContraAdmin)
+admin.site.register(Balance, BalanceAdmin)
